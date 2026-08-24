d4702720fb415ca5530debf79089a974e20e2638
Tran Van Ket <tvket2402@gmail.com>
2026-08-24 08:57:11 +0700

    ok

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config({ quiet: true });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello from Node.js server!');
 });
 
+app.post('/api/post', (req, res) => {
+    const param = req.query.id;
+    const body = req.body;
+    const headers = req.headers;
+    const idHeader = req.headers['idheader'];
+
+    console.log('all data and Log:', param, body, headers, idHeader);
+
+    res.json({
+        message: 'This is a POST request test!',
+        param,
+        body,
+        idHeader,
+        headers
+    });
+});
+
 const port = 3000;
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
